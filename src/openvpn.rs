5307e044b2376a18089e9115a2932a994589e228
Tommaso Thea Cioni <zacktommy1118@gmail.com>
2022-09-19 21:14:10 +0200

    Stopped writing configuration files to disk. The settings are passed via command line arguments now.

--- src/openvpn.rs
+++ src/openvpn.rs
@@ -1,23 +1,28 @@
-use std::process::Command;
+use tokio::process::Command;
 
-/// Generates an OpenVPN config file, writes it to disk and returns its path.
-pub fn generate_openvpn_configuration(ip: &str) -> &str {
-    let template = include_str!("template.ovpn");
-
-    let config = template.replace("{ip}", ip);
-
-    let path = "uk.ovpn";
-    std::fs::write(path, config).expect("Could not save the template.");
-
-    path
-}
-
-/// Runs the OpenVPN client to connect.
-pub fn run_openvpn(path: &str) {
+/// Runs the OpenVPN client to connect, the arguments are passed via command line without any configuration file.
+pub async fn run_openvpn(ip: &str) {
     let mut c = Command::new("sudo")
-        .args(["openvpn", "--config", path])
+        .args([
+            "openvpn",
+            "--remote",
+            ip,
+            "--nobind",
+            "--dev",
+            "tun",
+            "--secret",
+            "auth.key",
+            "--cipher",
+            "AES-256-CBC",
+            "--ifconfig",
+            "10.8.0.1",
+            "10.8.0.2",
+            "--redirect-gateway",
+            "def1",
+            "--auth-nocache",
+        ])
         .spawn()
         .expect("Couldn't spawn openvpn client.");
 
-    c.wait().unwrap();
+    c.wait().await.unwrap();
 }
