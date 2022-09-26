81ceb634d2ee2180510c519e94d1355281c0eed7
Tommaso Thea Cioni <zacktommy1118@gmail.com>
2022-09-26 19:32:18 +0200

    Removed security group rules' cidr limiting as the rules accumulated with every IP change. I think using a new OpenVPN key everytime is enough.

--- src/security_groups.rs
+++ src/security_groups.rs
@@ -1,24 +1,12 @@
 use aws_sdk_ec2::Client;
 
-
 pub async fn configure_security_group(client: &Client) {
-    // TODO check if it's failing because the group already exists, or because of other errors.
-    // If it already exists, no problem.
     let _ = client
         .create_security_group()
         .group_name("globevpn")
         .description("Security group for GlobeVPN.")
         .send()
         .await;
-
-    let cidr = public_ip::addr_v4()
-        .await
-        .map(|ip| format!("{ip}/32"))
-        .unwrap_or_else(|| {
-            println!("Couldn't retrieve your public ip address.");
-            println!("Enabling all IP address, be wary.");
-            "0.0.0.0/0".to_string()
-        });
 
     // TODO check if it's failing because the rule already exists, or because of other errors.
     // If it already exists, no problem.
@@ -28,7 +16,7 @@ pub async fn configure_security_group(client: &Client) {
         .authorize_security_group_ingress()
         .group_name("globevpn")
         .ip_protocol("tcp")
-        .cidr_ip(&cidr)
+        .cidr_ip("0.0.0.0/0")
         .from_port(22)
         .to_port(22)
         .send()
@@ -38,7 +26,7 @@ pub async fn configure_security_group(client: &Client) {
         .authorize_security_group_ingress()
         .group_name("globevpn")
         .ip_protocol("udp")
-        .cidr_ip(&cidr)
+        .cidr_ip("0.0.0.0/0")
         .from_port(1194)
         .to_port(1194)
         .send()
